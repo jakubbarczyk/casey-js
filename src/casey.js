a38e91bb317aa97e1ecb192e340931f2d51aadf9
Jakub Barczyk <jakubbarczyk@yahoo.com>
2016-10-15 18:26:34 +0200

    Make exported method references constant

--- src/casey.js
+++ src/casey.js
@@ -3,46 +3,65 @@
 import * as _case from "./case";
 import * as _pattern from "./pattern";
 
-export function isString(arg) {
+export const isString = _isString;
+export const isLowerCase = _isLowerCase;
+export const isUpperCase = _isUpperCase;
+export const isCamelCase = _isCamelCase;
+export const isPascalCase = _isPascalCase;
+export const isKebabCase = _isKebabCase;
+export const isSnakeCase = _isSnakeCase;
+export const isTrainCase = _isTrainCase;
+
+export const caseOf = _caseOf;
+
+export const toLowerCase = _toLowerCase;
+export const toUpperCase = _toUpperCase;
+export const toCamelCase = _toCamelCase;
+export const toPascalCase = _toPascalCase;
+export const toKebabCase = _toKebabCase;
+export const toSnakeCase = _toSnakeCase;
+export const toTrainCase = _toTrainCase;
+
+function _isString(arg) {
     return typeof arg === "string" || arg !== null && typeof arg === "object" && arg.constructor === String;
 }
 
-export function isLowerCase(str) {
+function _isLowerCase(str) {
     checkForTypeError("isLowerCase", str);
     return _pattern.LOWER_CASE.test(str);
 }
 
-export function isUpperCase(str) {
+function _isUpperCase(str) {
     checkForTypeError("isUpperCase", str);
     return _pattern.UPPER_CASE.test(str);
 }
 
-export function isCamelCase(str) {
+function _isCamelCase(str) {
     checkForTypeError("isCamelCase", str);
     return _pattern.CAMEL_CASE.test(str);
 }
 
-export function isPascalCase(str) {
+function _isPascalCase(str) {
     checkForTypeError("isPascalCase", str);
     return _pattern.PASCAL_CASE.test(str);
 }
 
-export function isKebabCase(str) {
+function _isKebabCase(str) {
     checkForTypeError("isKebabCase", str);
     return _pattern.KEBAB_CASE.test(str);
 }
 
-export function isSnakeCase(str) {
+function _isSnakeCase(str) {
     checkForTypeError("isSnakeCase", str);
     return _pattern.SNAKE_CASE.test(str);
 }
 
-export function isTrainCase(str) {
+function _isTrainCase(str) {
     checkForTypeError("isTrainCase", str);
     return _pattern.TRAIN_CASE.test(str);
 }
 
-export function caseOf(str) {
+function _caseOf(str) {
     checkForTypeError("caseOf", str);
 
     if (isCamelCase(str)) return _case.CAMEL_CASE;
@@ -56,39 +75,51 @@ export function caseOf(str) {
     return _case.UNDEFINED_CASE;
 }
 
-export function toLowerCase(str) {
+function _toLowerCase(str) {
     if (isLowerCase(str)) return str;
     return String.prototype.toLowerCase.call(str);
 }
 
-export function toUpperCase(str) {
+function _toUpperCase(str) {
     if (isUpperCase(str)) return str;
     return String.prototype.toUpperCase.call(str);
 }
 
-export function toCamelCase(str) {
+function _toCamelCase(str) {
     if (isCamelCase(str)) return str;
     return fragment(toLowerCase(str)).map((frag, idx) => idx === 0 ? frag : toUpperCase(frag[0]) + tail(frag)).join("");
 }
 
-export function toPascalCase(str) {
+function _toPascalCase(str) {
     if (isPascalCase(str)) return str;
     return fragment(toLowerCase(str)).map((frag) => toUpperCase(frag[0]) + tail(frag)).join("");
 }
 
-export function toKebabCase(str) {
+function _toKebabCase(str) {
     if (isKebabCase(str)) return str;
     return fragment(toLowerCase(str)).join("-");
 }
 
-export function toSnakeCase(str) {
+function _toSnakeCase(str) {
     if (isSnakeCase(str)) return str;
     return fragment(toLowerCase(str)).join("_");
 }
 
-export function toTrainCase(str) {
+function _toTrainCase(str) {
     if (isTrainCase(str)) return str;
     return fragment(toUpperCase(str)).join("-");
+}
+
+function tail(str) {
+    return String.prototype.slice.call(str, 1);
+}
+
+function fragment(str) {
+    return String.prototype.match.call(str, _pattern.FRAGMENT)
+}
+
+function checkForTypeError(fnName, arg) {
+    if (!isString(arg)) throw new TypeError(`${fnName} argument ${arg} is not a string`);
 }
 
 function tail(str) {
