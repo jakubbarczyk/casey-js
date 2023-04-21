b44470b0ae0c39def3bc6984e5913007edfcf436
Jakub Barczyk <jakubbarczyk@yahoo.com>
2023-04-21 10:13:55 +0200

    Regenerate package lock

--- dist/casey.js
+++ dist/casey.js
@@ -1,8 +1,119 @@
-parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcelRequire,u="function"==typeof require&&require;function f(t,n){if(!r[t]){if(!e[t]){var i="function"==typeof parcelRequire&&parcelRequire;if(!n&&i)return i(t,!0);if(o)return o(t,!0);if(u&&"string"==typeof t)return u(t);var c=new Error("Cannot find module '"+t+"'");throw c.code="MODULE_NOT_FOUND",c}p.resolve=function(r){return e[t][1][r]||r},p.cache={};var l=r[t]=new f.Module(t);e[t][0].call(l.exports,p,l,l.exports,this)}return r[t].exports;function p(e){return f(p.resolve(e))}}f.isParcelRequire=!0,f.Module=function(e){this.id=e,this.bundle=f,this.exports={}},f.modules=e,f.cache=r,f.parent=o,f.register=function(r,t){e[r]=[function(e,r){r.exports=t},{}]};for(var c=0;c<t.length;c++)try{f(t[c])}catch(e){i||(i=e)}if(t.length){var l=f(t[t.length-1]);"object"==typeof exports&&"undefined"!=typeof module?module.exports=l:"function"==typeof define&&define.amd?define(function(){return l}):n&&(this[n]=l)}if(parcelRequire=f,i)throw i;return f}({"pQ2Q":[function(require,module,exports) {
-"use strict";Object.defineProperty(exports,"__esModule",{value:!0}),exports.TRAIN_CASE=exports.SNAKE_CASE=exports.KEBAB_CASE=exports.PASCAL_CASE=exports.CAMEL_CASE=exports.UPPER_CASE=exports.LOWER_CASE=void 0;var A="LOWER_CASE";exports.LOWER_CASE=A;var E="UPPER_CASE";exports.UPPER_CASE=E;var C="CAMEL_CASE";exports.CAMEL_CASE=C;var S="PASCAL_CASE";exports.PASCAL_CASE=S;var r="KEBAB_CASE";exports.KEBAB_CASE=r;var e="SNAKE_CASE";exports.SNAKE_CASE=e;var _="TRAIN_CASE";exports.TRAIN_CASE=_;
-},{}],"UWTZ":[function(require,module,exports) {
-"use strict";Object.defineProperty(exports,"__esModule",{value:!0}),exports.FRAGMENT=exports.TRAIN_CASE=exports.SNAKE_CASE=exports.KEBAB_CASE=exports.PASCAL_CASE=exports.CAMEL_CASE=exports.UPPER_CASE=exports.LOWER_CASE=void 0;var A=/^[^A-Z]+$/;exports.LOWER_CASE=A;var r=/^[^a-z]+$/;exports.UPPER_CASE=r;var E=/^[a-z]+(?:[A-Z][a-z\d]+)*$/;exports.CAMEL_CASE=E;var e=/^[A-Z][a-z\d]+(?:[A-Z][a-z\d]+)*$/;exports.PASCAL_CASE=e;var a=/^[a-z][a-z\d]+(-[a-z\d]+)+$/;exports.KEBAB_CASE=a;var t=/^[a-z][a-z\d]+(_[a-z\d]+)+$/;exports.SNAKE_CASE=t;var o=/^[A-Z][a-z\d]+(-[A-Z][a-z\d]+)+$/;exports.TRAIN_CASE=o;var s=/([A-Za-z\d]+)+/g;exports.FRAGMENT=s;
-},{}],"UrjT":[function(require,module,exports) {
-"use strict";Object.defineProperty(exports,"__esModule",{value:!0}),exports.toTrainCase=exports.toSnakeCase=exports.toKebabCase=exports.toPascalCase=exports.toCamelCase=exports.toUpperCase=exports.toLowerCase=exports.caseOf=exports.isTrainCase=exports.isSnakeCase=exports.isKebabCase=exports.isPascalCase=exports.isCamelCase=exports.isUpperCase=exports.isLowerCase=exports.isString=void 0;var e=o(require("./case")),t=o(require("./pattern"));function r(){if("function"!=typeof WeakMap)return null;var e=new WeakMap;return r=function(){return e},e}function o(e){if(e&&e.__esModule)return e;if(null===e||"object"!=typeof e&&"function"!=typeof e)return{default:e};var t=r();if(t&&t.has(e))return t.get(e);var o={},n=Object.defineProperty&&Object.getOwnPropertyDescriptor;for(var s in e)if(Object.prototype.hasOwnProperty.call(e,s)){var a=n?Object.getOwnPropertyDescriptor(e,s):null;a&&(a.get||a.set)?Object.defineProperty(o,s,a):o[s]=e[s]}return o.default=e,t&&t.set(e,o),o}function n(e){return(n="function"==typeof Symbol&&"symbol"==typeof Symbol.iterator?function(e){return typeof e}:function(e){return e&&"function"==typeof Symbol&&e.constructor===Symbol&&e!==Symbol.prototype?"symbol":typeof e})(e)}var s=g;exports.isString=s;var a=_(t.LOWER_CASE);exports.isLowerCase=a;var i=_(t.UPPER_CASE);exports.isUpperCase=i;var p=_(t.CAMEL_CASE);exports.isCamelCase=p;var u=_(t.PASCAL_CASE);exports.isPascalCase=u;var c=_(t.KEBAB_CASE);exports.isKebabCase=c;var C=_(t.SNAKE_CASE);exports.isSnakeCase=C;var f=_(t.TRAIN_CASE);exports.isTrainCase=f;var l=P;exports.caseOf=l;var S=m;exports.toLowerCase=S;var x=j;exports.toUpperCase=x;var y=O;exports.toCamelCase=y;var A=L;exports.toPascalCase=A;var E=w;exports.toKebabCase=E;var v=d;exports.toSnakeCase=v;var b=K;function g(e){return"string"==typeof e||null!==e&&"object"===n(e)&&e.constructor===String}function _(e){return function(t){return!!s(t)&&e.test(t)}}function P(t){return p(t)?e.CAMEL_CASE:u(t)?e.PASCAL_CASE:c(t)?e.KEBAB_CASE:C(t)?e.SNAKE_CASE:f(t)?e.TRAIN_CASE:a(t)?e.LOWER_CASE:i(t)?e.UPPER_CASE:null}function m(e){return String.prototype.toLowerCase.call(String(e))}function j(e){return String.prototype.toUpperCase.call(String(e))}function O(e){return R(m(String(e))).map(function(e,t){return 0===t?e:j(e[0])+M(e)}).join("")}function L(e){return R(m(String(e))).map(function(e){return j(e[0])+M(e)}).join("")}function w(e){return R(m(String(e))).join("-")}function d(e){return R(m(String(e))).join("_")}function K(e){return R(S(String(e))).map(function(e){return x(e[0])+M(e)}).join("-")}function M(e){return String.prototype.slice.call(e,1)}function R(e){return String.prototype.match.call(e,t.FRAGMENT)}exports.toTrainCase=b;
-},{"./case":"pQ2Q","./pattern":"UWTZ"}]},{},["UrjT"], "casey")
-//# sourceMappingURL=/casey.js.map
+(function (global, factory) {
+    typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
+    typeof define === 'function' && define.amd ? define(['exports'], factory) :
+    (global = typeof globalThis !== 'undefined' ? globalThis : global || self, factory(global.casey = {}));
+})(this, (function (exports) { 'use strict';
+
+    const LOWER_CASE$1 = 'LOWER_CASE';
+    const UPPER_CASE$1 = 'UPPER_CASE';
+    const CAMEL_CASE$1 = 'CAMEL_CASE';
+    const PASCAL_CASE$1 = 'PASCAL_CASE';
+    const KEBAB_CASE$1 = 'KEBAB_CASE';
+    const SNAKE_CASE$1 = 'SNAKE_CASE';
+    const TRAIN_CASE$1 = 'TRAIN_CASE';
+
+    const LOWER_CASE = /^[^A-Z]+$/;
+    const UPPER_CASE = /^[^a-z]+$/;
+    const CAMEL_CASE = /^[a-z]+(?:[A-Z][a-z\d]+)*$/;
+    const PASCAL_CASE = /^[A-Z][a-z\d]+(?:[A-Z][a-z\d]+)*$/;
+    const KEBAB_CASE = /^[a-z][a-z\d]+(-[a-z\d]+)+$/;
+    const SNAKE_CASE = /^[a-z][a-z\d]+(_[a-z\d]+)+$/;
+    const TRAIN_CASE = /^[A-Z][a-z\d]+(-[A-Z][a-z\d]+)+$/;
+    const FRAGMENT = /([A-Za-z\d]+)+/g;
+
+    const isString = _isString;
+
+    const isLowerCase = _is(LOWER_CASE);
+    const isUpperCase = _is(UPPER_CASE);
+    const isCamelCase = _is(CAMEL_CASE);
+    const isPascalCase = _is(PASCAL_CASE);
+    const isKebabCase = _is(KEBAB_CASE);
+    const isSnakeCase = _is(SNAKE_CASE);
+    const isTrainCase = _is(TRAIN_CASE);
+
+    const caseOf = _caseOf;
+
+    const toLowerCase = _toLowerCase;
+    const toUpperCase = _toUpperCase;
+    const toCamelCase = _toCamelCase;
+    const toPascalCase = _toPascalCase;
+    const toKebabCase = _toKebabCase;
+    const toSnakeCase = _toSnakeCase;
+    const toTrainCase = _toTrainCase;
+
+    function _isString(any) {
+        return typeof any === 'string' || any !== null && typeof any === 'object' && any.constructor === String;
+    }
+
+    function _is(pattern) {
+        return function (str) {
+            return isString(str) ? pattern.test(str) : false;
+        };
+    }
+
+    function _caseOf(str) {
+        if (isCamelCase(str)) return CAMEL_CASE$1;
+        else if (isPascalCase(str)) return PASCAL_CASE$1;
+        else if (isKebabCase(str)) return KEBAB_CASE$1;
+        else if (isSnakeCase(str)) return SNAKE_CASE$1;
+        else if (isTrainCase(str)) return TRAIN_CASE$1;
+        else if (isLowerCase(str)) return LOWER_CASE$1;
+        else if (isUpperCase(str)) return UPPER_CASE$1;
+        else return null;
+    }
+
+    function _toLowerCase(str) {
+        return String.prototype.toLowerCase.call(String(str));
+    }
+
+    function _toUpperCase(str) {
+        return String.prototype.toUpperCase.call(String(str));
+    }
+
+    function _toCamelCase(str) {
+        return _fragment(_toLowerCase(String(str))).map((frag, idx) => idx === 0 ? frag : _toUpperCase(frag[0]) + _tail(frag)).join('');
+    }
+
+    function _toPascalCase(str) {
+        return _fragment(_toLowerCase(String(str))).map(frag => _toUpperCase(frag[0]) + _tail(frag)).join('');
+    }
+
+    function _toKebabCase(str) {
+        return _fragment(_toLowerCase(String(str))).join('-');
+    }
+
+    function _toSnakeCase(str) {
+        return _fragment(_toLowerCase(String(str))).join('_');
+    }
+
+    function _toTrainCase(str) {
+        return _fragment(toLowerCase(String(str))).map(frag => toUpperCase(frag[0]) + _tail(frag)).join('-');
+    }
+
+    function _tail(str) {
+        return String.prototype.slice.call(str, 1);
+    }
+
+    function _fragment(str) {
+        return String.prototype.match.call(str, FRAGMENT);
+    }
+
+    exports.caseOf = caseOf;
+    exports.isCamelCase = isCamelCase;
+    exports.isKebabCase = isKebabCase;
+    exports.isLowerCase = isLowerCase;
+    exports.isPascalCase = isPascalCase;
+    exports.isSnakeCase = isSnakeCase;
+    exports.isString = isString;
+    exports.isTrainCase = isTrainCase;
+    exports.isUpperCase = isUpperCase;
+    exports.toCamelCase = toCamelCase;
+    exports.toKebabCase = toKebabCase;
+    exports.toLowerCase = toLowerCase;
+    exports.toPascalCase = toPascalCase;
+    exports.toSnakeCase = toSnakeCase;
+    exports.toTrainCase = toTrainCase;
+    exports.toUpperCase = toUpperCase;
+
+}));
+//# sourceMappingURL=casey.js.map
